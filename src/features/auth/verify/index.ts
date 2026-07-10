46804de0ba7727b2e42745b5759d981803577009
Юдешко Максим Юрьевич (4069343) <udeshko@dev.vtb.ru>
2026-07-10 16:25:01 +0300

    - useRegister → useVerify, UseRegisterResult → UseVerifyResult

--- src/features/auth/verify/index.ts
+++ src/features/auth/verify/index.ts
@@ -1,2 +1,2 @@
-export { useRegister } from "./model/useVerify";
-export type { UseRegisterResult } from "./model/types";
+export { useVerify } from "./model/useVerify";
+export type { UseVerifyResult } from "./model/types";
