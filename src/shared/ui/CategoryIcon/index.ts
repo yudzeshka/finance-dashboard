fcc9d1d18b61be5c8bc173f34fd44e519c3baada
Юдешко Максим Юрьевич (4069343) <udeshko@dev.vtb.ru>
2026-08-04 20:15:37 +0300

    feat(shared): add CategoryIcon component with inline SVG set and emoji mapping

---
+++ src/shared/ui/CategoryIcon/index.ts
@@ -0,0 +1,6 @@
+export { CategoryIcon } from "./CategoryIcon";
+export type { CategoryIconProps } from "./CategoryIcon";
+export { CategoryIconPicker } from "./CategoryIconPicker";
+export type { CategoryIconPickerProps } from "./CategoryIconPicker";
+export { categoryIcons } from "./icons";
+export { emojiToKey, resolveIconKey } from "./emojiMapping";
